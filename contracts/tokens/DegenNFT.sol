d234fe779094d4b4bdd6d3754ddb5fe3d6894e99
apeir99n <ilgiz.gimaltdinov@gmail.com>
2022-10-25 20:34:08 +0400

    fixed license

--- contracts/tokens/DegenNFT.sol
+++ contracts/tokens/DegenNFT.sol
@@ -1,4 +1,7 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: BUSL-1.1
+// Gearbox Protocol. Generalized leverage for DeFi protocols
+// (c) Gearbox Holdings, 2022
+
 pragma solidity ^0.8.10;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
