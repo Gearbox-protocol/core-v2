f8de95a6a122a2b5d96b970abe2189a798d52938
apeir99n <ilgiz.gimaltdinov@gmail.com>
2022-10-25 20:20:33 +0400

    updated /interfaces licenses to MIT

--- contracts/interfaces/ILPPriceFeed.sol
+++ contracts/interfaces/ILPPriceFeed.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 // Gearbox Protocol. Generalized leverage for DeFi protocols
 // (c) Gearbox Holdings, 2022
 pragma solidity ^0.8.10;
--- contracts/interfaces/IPriceOracle.sol
+++ contracts/interfaces/IPriceOracle.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: MIT
 // Gearbox Protocol. Generalized leverage for DeFi protocols
 // (c) Gearbox Holdings, 2022
 pragma solidity ^0.8.10;
