443a1692a151cb83bf13698a73109e1c9a715af6
Van0k <ivkamakin@gmail.com>
2022-10-25 21:29:34 +0400

    chore: license updates

--- contracts/adapters/UniversalAdapter.sol
+++ contracts/adapters/UniversalAdapter.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: BUSL-1.1
 // Gearbox Protocol. Generalized leverage for DeFi protocols
 // (c) Gearbox Holdings, 2022
 pragma solidity ^0.8.10;
