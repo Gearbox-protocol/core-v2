d8a3e226a16801b65635f5eb6e3e5be5350bbc8f
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-03-09 15:15:57 +0200

    feat: upgrade universal adapter

--- contracts/adapters/UniversalAdapter.sol
+++ contracts/adapters/UniversalAdapter.sol
@@ -1,65 +1,34 @@
 // SPDX-License-Identifier: BUSL-1.1
 // Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Holdings, 2022
-pragma solidity ^0.8.10;
+// (c) Gearbox Holdings, 2023
+pragma solidity ^0.8.17;
 
-import {IUniversalAdapter, RevocationPair} from "../interfaces/adapters/IUniversalAdapter.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {AbstractAdapter} from "./AbstractAdapter.sol";
 import {AdapterType} from "../interfaces/adapters/IAdapter.sol";
 import {ICreditManagerV2} from "../interfaces/ICreditManagerV2.sol";
-import {ZeroAddressException} from "../interfaces/IErrors.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IUniversalAdapter, RevocationPair} from "../interfaces/adapters/IUniversalAdapter.sol";
 import {UNIVERSAL_CONTRACT} from "../libraries/Constants.sol";
+import {ZeroAddressException} from "../interfaces/IErrors.sol";
 
-/// @title UniversalAdapter
-/// @dev Implements the initial version of universal adapter, which handles allowance revocations
-contract UniversalAdapter is IUniversalAdapter {
-    /// @dev The target contract, which is always the same special address for the universal adapter
-    address public immutable targetContract = UNIVERSAL_CONTRACT;
+/// @title Universal adapter
+/// @notice Implements the initial version of universal adapter, which handles allowance revocations
+contract UniversalAdapter is AbstractAdapter, IUniversalAdapter {
+    AdapterType public constant override _gearboxAdapterType = AdapterType.UNIVERSAL;
+    uint16 public constant override _gearboxAdapterVersion = 2;
 
-    /// @dev The credit manager this universal adapter connects to
-    ICreditManagerV2 public immutable override creditManager;
+    /// @notice Constructor
+    /// @param _creditManager Credit manager address
+    /// @dev Target contract is always the same special address
+    constructor(address _creditManager) AbstractAdapter(_creditManager, UNIVERSAL_CONTRACT) {}
 
-    AdapterType public constant _gearboxAdapterType = AdapterType.UNIVERSAL;
-    uint16 public constant _gearboxAdapterVersion = 1;
+    /// @notice Revokes adapters allowances for specified tokens of the credit account
+    /// @param revocations Adapter/token pairs to revoke allowances for
+    function revokeAdapterAllowances(RevocationPair[] calldata revocations) external creditFacadeOnly {
+        address creditAccount = _creditAccount();
 
-    /// @dev Constructor
-    /// @param _creditManager Address of the Credit Manager
-    constructor(address _creditManager) {
-        if (_creditManager == address(0)) revert ZeroAddressException();
-        creditManager = ICreditManagerV2(_creditManager);
-    }
-
-    /// @dev Sets allowances to zero for the provided spender/token pairs, for msg.sender's CA
-    /// @param revocations Pairs of spenders/tokens to revoke allowances for
-    function revokeAdapterAllowances(RevocationPair[] calldata revocations) external {
-        address creditAccount = creditManager.getCreditAccountOrRevert(msg.sender);
-
-        _revokeAdapterAllowances(revocations, creditAccount);
-    }
-
-    /// @dev Sets allowances to zero for the provided spender/token pairs
-    /// Checks that the msg.sender CA matches the expected account, since the
-    /// Provided revocations can be specific to a particular CA
-    /// @param revocations Pairs of spenders/tokens to revoke allowances for
-    /// @param expectedCreditAccount Credit account that msg.sender is expected to have
-    function revokeAdapterAllowances(RevocationPair[] calldata revocations, address expectedCreditAccount) external {
-        address creditAccount = creditManager.getCreditAccountOrRevert(msg.sender);
-
-        if (creditAccount != expectedCreditAccount) {
-            revert UnexpectedCreditAccountException(expectedCreditAccount, creditAccount);
-        }
-
-        _revokeAdapterAllowances(revocations, creditAccount);
-    }
-
-    /// @dev Internal implementation for allowance revocations
-    /// Checks that there are no zero addresses in a pair and sets allowance to 1
-    /// through CreditManager.approveCreditAccount
-    /// @param revocations Pairs of spenders/tokens to revoke allowances for
-    /// @param creditAccount Credit account to revoke allowances for
-    function _revokeAdapterAllowances(RevocationPair[] calldata revocations, address creditAccount) internal {
         uint256 numRevocations = revocations.length;
-
         for (uint256 i; i < numRevocations;) {
             address spender = revocations[i].spender;
             address token = revocations[i].token;
@@ -69,7 +38,6 @@ contract UniversalAdapter is IUniversalAdapter {
             }
 
             uint256 allowance = IERC20(token).allowance(creditAccount, spender);
-
             if (allowance > 1) {
                 creditManager.approveCreditAccount(spender, token, 1);
             }
@@ -80,22 +48,14 @@ contract UniversalAdapter is IUniversalAdapter {
         }
     }
 
-    /// @notice For demonstration purposes only. Not in V2 launch scope
-    ///
-    // function withdraw(address token, uint256 amount) external {
-    //     address creditAccount = creditManager.getCreditAccountOrRevert(
-    //         msg.sender
-    //     );
-
-    //     if (creditManager.tokenMasksMap(token) == 0)
-    //         revert("Token contract is not allowed");
-
-    //     creditManager.executeOrder(
-    //         msg.sender,
-    //         token,
-    //         abi.encodeWithSelector(IERC20.transfer.selector, msg.sender, amount)
-    //     );
-
-    //     creditManager.fullCollateralCheck(creditAccount);
+    // /// @notice Withdraws given amount of token from the credit account to the specified address
+    // /// @param token Address of the token to withdraw (must be collateral token in the credit manager)
+    // /// @param to Token recipient
+    // /// @param amount Amount to withdraw
+    // function withdrawTo(address token, address to, uint256 amount) external creditFacadeOnly {
+    //     if (creditManager.tokenMasksMap(token) == 0) {
+    //         revert("Token is not allowed");
+    //     }
+    //     creditManager.executeOrder(token, abi.encodeCall(IERC20.transfer, (to, amount)));
     // }
 }
--- contracts/interfaces/adapters/IUniversalAdapter.sol
+++ contracts/interfaces/adapters/IUniversalAdapter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Holdings, 2022
-pragma solidity ^0.8.10;
+// (c) Gearbox Holdings, 2023
+pragma solidity ^0.8.17;
 
 import {IAdapter} from "./IAdapter.sol";
 
@@ -10,20 +10,10 @@ struct RevocationPair {
     address token;
 }
 
-interface IUniversalAdapterExceptions {
-    /// @dev Thrown when the Credit Account of msg.sender does not match the provided expected account
-    error UnexpectedCreditAccountException(address expected, address actual);
-}
-
-interface IUniversalAdapter is IAdapter, IUniversalAdapterExceptions {
-    /// @dev Sets allowances to zero for provided spender/token pairs, for msg.sender's CA
-    /// @param revocations Pairs of spenders/tokens to revoke allowances for
+/// @title Universal adapter interface
+/// @notice Implements the initial version of universal adapter, which handles allowance revocations
+interface IUniversalAdapter is IAdapter {
+    /// @notice Revokes adapters allowances for specified tokens of the credit account
+    /// @param revocations Adapter/token pairs to revoke allowances for
     function revokeAdapterAllowances(RevocationPair[] calldata revocations) external;
-
-    /// @dev Sets allowances to zero for the provided spender/token pairs
-    /// Checks that the msg.sender CA matches the expected account, since
-    /// provided revocations are specific to a particular CA
-    /// @param revocations Pairs of spenders/tokens to revoke allowances for
-    /// @param expectedCreditAccount Credit account that msg.sender is expected to have
-    function revokeAdapterAllowances(RevocationPair[] calldata revocations, address expectedCreditAccount) external;
 }
