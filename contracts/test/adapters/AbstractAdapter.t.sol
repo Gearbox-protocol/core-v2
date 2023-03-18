91547b513c864fb844f4cc5471131b7c88e1c891
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-03-18 15:36:01 +0200

    feat: _executeSwapNoApprove reverts on bad tokens

--- contracts/test/adapters/AbstractAdapter.t.sol
+++ contracts/test/adapters/AbstractAdapter.t.sol
@@ -634,4 +634,63 @@ contract AbstractAdapterTest is
             );
         }
     }
+
+    /// @dev [AA-15]: _executeSwapNoApprove reverts if tokenIn or tokenOut are not allowed
+    function test_AA_15_executeSwap_reverts_if_tokenIn_or_tokenOut_are_not_allowed()
+        public
+    {
+        _openTestCreditAccount();
+
+        address TOKEN = address(0xdead);
+        bytes memory DUMB_CALLDATA = abi.encodeWithSignature(
+            "hello(string)",
+            "world"
+        );
+
+        // ti == 0 => bad tokenOut, ti == 1 => bad tokenIn
+        // sa == 0 => no approve, sa == 1 => safe approve
+        for (uint256 ti; ti < 2; ++ti) {
+            for (uint256 sa; sa < 2; ++sa) {
+                bytes memory callData;
+                if (sa == 1) {
+                    callData = abi.encodeWithSelector(
+                        adapterMock.executeSwapSafeApprove.selector,
+                        ti == 1 ? TOKEN : dai,
+                        ti == 1 ? dai : TOKEN,
+                        DUMB_CALLDATA,
+                        false
+                    );
+                } else {
+                    callData = abi.encodeWithSelector(
+                        adapterMock.executeSwapNoApprove.selector,
+                        ti == 1 ? TOKEN : dai,
+                        ti == 1 ? dai : TOKEN,
+                        DUMB_CALLDATA,
+                        false
+                    );
+                }
+
+                if (sa == 0 && ti == 1) {
+                    evm.expectRevert(
+                        abi.encodeWithSelector(
+                            IAdapterExceptions.TokenIsNotInAllowedList.selector,
+                            TOKEN
+                        )
+                    );
+                } else {
+                    evm.expectRevert(TokenNotAllowedException.selector);
+                }
+
+                evm.prank(USER);
+                creditFacade.multicall(
+                    multicallBuilder(
+                        MultiCall({
+                            target: address(adapterMock),
+                            callData: callData
+                        })
+                    )
+                );
+            }
+        }
+    }
 }
