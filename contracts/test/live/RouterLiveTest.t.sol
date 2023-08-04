f5577e70a1a49df4f4cac06553614ddf23fbf223
doomsower <K.Kuznetcov@gmail.com>
2023-08-04 12:52:01 -0400

    test: hardcode exceptions

--- contracts/test/live/RouterLiveTest.t.sol
+++ contracts/test/live/RouterLiveTest.t.sol
@@ -56,6 +56,9 @@ contract RouterLiveTest is Test {
     using MultiCallOps for MultiCall[];
     using AddressList for address[];
 
+    // cm -> tokens
+    mapping(address => mapping(address => bool)) expectedReverts;
+
     IAddressProvider addressProvider;
     IDataCompressor dataCompressor;
     IPriceOracleV2 oracle;
@@ -66,16 +69,82 @@ contract RouterLiveTest is Test {
         dataCompressor = IDataCompressor(addressProvider.getDataCompressor());
         router = IRouter(addressProvider.getLeveragedActions());
         oracle = IPriceOracleV2(addressProvider.getPriceOracle());
+
+        // DAI
+        expectedReverts[0x672461Bfc20DD783444a830Ad4c38b345aB6E2f7][
+            0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B
+        ] = true; // cvx
+        // USDC
+        expectedReverts[0x95357303f995e184A7998dA6C6eA35cC728A1900][
+            0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B
+        ] = true; // cvx
+        // WETH
+        expectedReverts[0x5887ad4Cb2352E7F01527035fAa3AE0Ef2cE2b9B][
+            0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B
+        ] = true; // cvx
+        // wstETH
+        expectedReverts[0xe0bCE4460795281d39c91da9B0275BcA968293de][
+            0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd
+        ] = true; // gusd
+        expectedReverts[0xe0bCE4460795281d39c91da9B0275BcA968293de][
+            0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B
+        ] = true; // cvx
+        expectedReverts[0xe0bCE4460795281d39c91da9B0275BcA968293de][
+            0xD533a949740bb3306d119CC777fa900bA034cd52
+        ] = true; // crv
+        // WBTC
+        expectedReverts[0xc62BF8a7889AdF1c5Dc4665486c7683ae6E74e0F][
+            0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B
+        ] = true; // cvx
+        expectedReverts[0xc62BF8a7889AdF1c5Dc4665486c7683ae6E74e0F][
+            0xD533a949740bb3306d119CC777fa900bA034cd52
+        ] = true; // crv
+        // FRAX
+        expectedReverts[0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8][
+            0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84
+        ] = true; // stETH
+        expectedReverts[0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8][
+            0xD533a949740bb3306d119CC777fa900bA034cd52
+        ] = true; // crv
+        expectedReverts[0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8][
+            0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B
+        ] = true; // cvx
+        expectedReverts[0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8][
+            0xaF314b088B53835d5cF4e4CB81beABa5934a61fe
+        ] = true; // stkcvxFRAX3CRV-f - pidToPhantomToken missing
+        expectedReverts[0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8][
+            0x0A1D4A25d0390899b90bCD22E1Ef155003EA76d7
+        ] = true; // stkcvxLUSD3CRV-f - pidToPhantomToken missing
+        expectedReverts[0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8][
+            0x7e1992A7F28dAA5f6a2d34e2cd40f962f37B172C
+        ] = true; // stkcvxcrvPlain3andSUSD - pidToPhantomToken missing
+        expectedReverts[0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8][
+            0xbAc7a431146aeAf3F57A16b9954f332Fd292F270
+        ] = true; // stkcvx3Crv - pidToPhantomToken missing
+        expectedReverts[0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8][
+            0x276187f24D41745513cbE2Bd5dFC33a4d8CDc9ed
+        ] = true; // stkcvxcrvFRAX - pidToPhantomToken missing
+        expectedReverts[0xA3E1e0d58FE8dD8C9dd48204699a1178f1B274D8][
+            0xe15B7D80a51e1fe54aC355CaBE848Efce5289BDB
+        ] = true; // stkcvxsteCRV - pidToPhantomToken missing
+    }
+
+    function test_live_router_can_open_ca() public {
+        CreditManagerData[] memory cmList = dataCompressor
+            .getCreditManagersList();
+        for (uint256 i = 0; i < cmList.length; ++i) {
+            if (cmList[i].version == 2) {
+                _testSingleCm(cmList[i]);
+            }
+        }
     }
 
     function _testSingleCm(CreditManagerData memory cmData) internal {
-        // if (cmData.addr == 0xe0bCE4460795281d39c91da9B0275BcA968293de) {
-        // emit log_string("skip wstETH");
-        // return;
-        // }
         ICreditManagerV2 cm = ICreditManagerV2(cmData.addr);
         ICreditFacade cf = ICreditFacade(cmData.creditFacade);
 
+        // allow USER to open CA
+        // this is not included in snapshot and not rolled back
         IDegenNFT dnft = IDegenNFT(cf.degenNFT());
         vm.prank(dnft.minter());
         dnft.mint(USER, 30);
@@ -90,21 +159,22 @@ contract RouterLiveTest is Test {
                 abi.encodePacked(
                     underlyingSymbol,
                     " ",
-                    Strings.toHexString(uint160(address(cm)), 20),
+                    Strings.toHexString(address(cm)),
                     " min borrow ",
                     Strings.toString(accountAmount)
                 )
             )
         );
 
+        // this is not included in snapshot and not rolled back
         _maybeTopUpPool(cmData);
 
+        // this is not included in snapshot and not rolled back
         deal(underlying, USER, 1000 * accountAmount, false);
         vm.prank(USER);
         IERC20(cmData.underlying).approve(cmData.addr, type(uint256).max);
 
         uint256 tokenCount = cm.collateralTokensCount();
-
         for (uint256 i = 0; i < tokenCount; ++i) {
             (address tokenOut, uint16 lt) = cm.collateralTokens(i);
 
@@ -121,7 +191,7 @@ contract RouterLiveTest is Test {
                     abi.encodePacked(
                         symbol,
                         " ",
-                        Strings.toHexString(uint160(tokenOut), 20),
+                        Strings.toHexString(tokenOut),
                         " lt ",
                         Strings.toString(lt)
                     )
@@ -144,10 +214,10 @@ contract RouterLiveTest is Test {
         // this is assuming lossless conversion
         // x = 1/(lt * (1-slippage)) - 1
         // uint256 collateral = ((PERCENTAGE_FACTOR * borrowed)) / lt - borrowed;
-        uint256 collateral = 1 * borrowed;
+        uint256 collateral = 5 * borrowed;
 
         Balance[] memory expectedBalances = _getBalances(cm);
-        expectedBalances.setBalance(cmData.underlying, collateral + borrowed); // borrowed + collateral
+        expectedBalances.setBalance(cmData.underlying, collateral + borrowed);
 
         address[] memory connectors = _getConnectors(cm);
 
@@ -174,43 +244,13 @@ contract RouterLiveTest is Test {
         uint256 tokenSnapshot = vm.snapshot();
 
         vm.prank(USER);
-        try cf.openCreditAccountMulticall(borrowed, USER, calls, 0) {
-            // emit log_string("success");
-            // TEMP
-            // CreditAccountData memory caData = dataCompressor
-            //     .getCreditAccountData(cmData.addr, USER);
-            // emit log_named_address("opened ca", caData.addr);
-            // emit log_named_uint("borrowedAmount", caData.borrowedAmount);
-            // emit log_named_uint("healthFactor", caData.healthFactor);
-            // uint256 tokenCount = cm.collateralTokensCount();
-            // for (uint256 i = 0; i < tokenCount; ++i) {
-            //     if (caData.balances[i].token == tokenOut) {
-            //         emit log_named_uint(
-            //             "target balance",
-            //             caData.balances[i].balance
-            //         );
-            //     }
-            // }
-        } catch Error(string memory _err) {
-            emit log_named_string("!revert", _err);
-        } catch (bytes memory _err) {
-            emit log_named_bytes("!revert", _err);
+        if (expectedReverts[cmData.addr][tokenOut]) {
+            vm.expectRevert();
+            emit log_string("reverted as expected");
         }
+        cf.openCreditAccountMulticall(borrowed, USER, calls, 0);
 
         vm.revertTo(tokenSnapshot);
-    }
-
-    function test_live_router_can_open_ca() public {
-        CreditManagerData[] memory cmList = dataCompressor
-            .getCreditManagersList();
-        uint256 cmSnapshot = vm.snapshot();
-
-        for (uint256 i = 0; i < cmList.length; ++i) {
-            if (cmList[i].version == 2) {
-                _testSingleCm(cmList[i]);
-                vm.revertTo(cmSnapshot);
-            }
-        }
     }
 
     // check available liquidity in pool for this cm and mock-deposit some if it's low
